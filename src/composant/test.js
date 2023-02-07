353a0c199ca95be7edeafd0ec0e728918ccad63e
YakareKoite <yakarekoite@gmail.com>
2023-02-07 19:08:31 +0100

    nouvelle version de AnimList

--- src/composant/test.js
+++ src/composant/test.js
@@ -1,32 +1,31 @@
-import { useParams } from 'react-router-dom'
-import React, { useState, useEffect } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
 import '../style.scss';
 import { useLocation } from "react-router-dom";
 
-const Test = (post) => {
-  const [favorites, setFavorites] = useState('');
-  const location = useLocation();
 
-  useEffect(() => {
-    if (location.state && location.state.setFavorites) {
-      setFavorites(location.state.favorites);
-    }
-  }, [location.state]);
-  console.log(location.state)
+//Les favoris
+const Test = (favorites) => {
+  //recuperation de la donnée
+  const location = useLocation();
+  const a = JSON.parse(localStorage.getItem('cle'))
+  console.log(a);
   return (
     <div>
-    <h2>My Favorite Anime Shows</h2>
-    <ul>
-      {favorites.map(( index, post ) => (
-        <img key={index} src={post}/>
-      ))}
-      <button ><Link to={`/`}  className='but' >Retourner au Catalogue </Link></button>
-    </ul>
+  {a.map((anime) => (
+  <d key={anime.id}>
+  <img src={anime.post} className="fav"/>
+  </d>
+))}
+<h3 className="mes">Mes favoris</h3>
+<button ><Link to={`/`}  className='bute' >Retourner au Catalogue </Link></button>
   </div>
-    )
+
+
+  )
+
 }
+
+
 
 export default Test
 
